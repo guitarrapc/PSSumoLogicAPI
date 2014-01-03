79c2eb1d5c28ddee61ba2edccfac90cdd5f53bb3
guitarrapc <cherishacquire@outlook.com>
2014-01-03 17:20:26 +0900

    change test message

--- PSSumoLogicAPI/Test/Test-GetSumoApiCollector.ps1
+++ PSSumoLogicAPI/Test/Test-GetSumoApiCollector.ps1
@@ -6,7 +6,6 @@ $host.Ui.WriteVerboseLine("Running Synchronize request to get collectors")
 $collectors = Get-PSSumoLogicApiCollector -Credential $credential
 $collectors
 
-# Obtain each Collectors
+# Obtain each Collectors for first 5
 $host.Ui.WriteVerboseLine("Running Synchronize request for each collectorId")
 Get-PSSumoLogicApiCollector -Credential $credential -CollectorIds ($collectors.Id | select -First 5)
-
--- PSSumoLogicAPI/Test/Test-GetSumoApiCollectorAsync.ps1
+++ PSSumoLogicAPI/Test/Test-GetSumoApiCollectorAsync.ps1
@@ -5,6 +5,6 @@ $credential = Get-PSSumoLogicApiCredential
 $host.Ui.WriteVerboseLine("Running Asynchronize request to get collectors")
 $collectors = Get-PSSumoLogicApiCollector -Credential $credential -Async
 
-# Obtain each Collectors
+# Obtain each Collectors for first 5
 $host.Ui.WriteVerboseLine("Running Asynchronize request for each CollectorId")
 Get-PSSumoLogicApiCollector -Credential $credential -CollectorIds ($collectors.Id | select -First 5) -Async
